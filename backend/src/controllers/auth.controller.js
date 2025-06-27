c6159a1e6730bc6010b132d6cca522ac48d05186
Urmila111 <basneturmi777@gmail.com>
2025-06-27 23:12:37 +0545

    fix bug

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -5,7 +5,7 @@ export const signup = async(req, res)=> {
     const{fullName, email, password} = req.body
     try {
        if (password.length < 6) {
-        return res.status(400).json({message: "Password must be at leaast 6 characters"});
+        return res.status(400).json({message: "Password must be at least 6 characters"});
        }
 
      const user = await User.findOne({email})
