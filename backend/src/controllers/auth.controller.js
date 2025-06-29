25c46d3e6309e6dfc8b071633e45de09ae731462
Urmila111 <basneturmi777@gmail.com>
2025-06-29 22:30:23 +0545

    Add basic login controller with email and password validation

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -46,10 +46,17 @@ export const signup = async(req, res)=> {
     }
 };
 
-export const login = (req, res)=> {
+export const login = async(req, res)=> {
   const  {email, password} = req.body
     try {
-      const user = await User.findOne
+      const user = await User.findOne({email})
+      if(!user){
+        return res.status(400).json({message: "Invalid Credentials"})
+      }
+      const isPasswordCorrect = await bcrypt.compare(password, user.password)
+      if (!isPasswordCorrect) {
+        return res.status(400).json({message: "Invalid Credentials"})
+      }
     } catch (error) {
       
     }
