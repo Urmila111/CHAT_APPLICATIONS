44c4e009561ec063f2fda8bfea7e74733199938a
Urmila111 <basneturmi777@gmail.com>
2025-06-29 23:06:25 +0545

    feat: add 500 status error response for server-side failures

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -78,5 +78,6 @@ export const logout = (req, res)=> {
       
     } catch (error) {
       console.log("Error in logout controller", error.message);
+      res.status(500).json({message:"Internal Server Error;"})
     }
 }
