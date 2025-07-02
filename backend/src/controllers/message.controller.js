a01a5b812e086545a1e9be57c8b8453dda441732
Urmila111 <basneturmi777@gmail.com>
2025-07-02 09:12:03 +0545

    Add controllers to fetch sidebar users and start getMessages logic

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,28 @@
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
+
+export const getUsersForSidebar = async(req,res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({__id: {$ne: loggedInUserId}}).select("-password");
+
+    res.status(200).json(filteredUsers)
+  } catch (error) {
+    console.log("Error in getUsersForSidebar: ", error.message);
+    res.status(500).json({error: "Internal sever error"});
+  }
+
+};
+
+export const getMessages = async(req,res) => {
+    try {
+       const {id: userToChatId} = req.params
+       const senderId = req.user.__id;
+
+       const messages = await Message
+
+    } catch (error) {
+        
+    }
+}
+
