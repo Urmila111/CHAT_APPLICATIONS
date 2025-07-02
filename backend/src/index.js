69a5122dbe453c5652a4a9ab1e34e46c75f2764b
Urmila111 <basneturmi777@gmail.com>
2025-07-02 09:07:46 +0545

    Add messageRoutes import

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.routes.js";
+
 import { connectDB } from "./lib/db.js";
 
 dotenv.config()
@@ -12,6 +14,7 @@ const PORT = process.env.PORT;
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () =>{
     console.log(" the Server is running on PORT:", PORT);
