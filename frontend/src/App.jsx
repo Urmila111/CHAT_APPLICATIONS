ac990f77c38f4f5de2abaf120ee57befd0af747c
Urmila111 <basneturmi777@gmail.com>
2025-07-05 23:23:17 +0545

    feat(App): add initial App component with Tailwind CSS styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,12 @@
+// src/App.jsx
+import React from "react";
 
+const App = () => {
+  return (
+    <div className="text-red-500 text-2xl font-bold text-center mt-10">
+      Hello, Tailwind is working!
+    </div>
+  );
+};
+
+export default App;
