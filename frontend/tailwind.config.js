0b98fd76c6b8bd36fcc1e40b64e4ad3bf4468c0f
Urmila111 <basneturmi777@gmail.com>
2025-07-05 19:41:44 +0545

    feat(tailwind): add initial Tailwind CSS configuration file

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./src/**/*.{html,js,jsx,ts,tsx}", // Add jsx, ts, tsx for latest frameworks
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
