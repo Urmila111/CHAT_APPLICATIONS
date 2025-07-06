01c5dda1510a793de234d6f0e23025c32aa33289
Urmila111 <basneturmi777@gmail.com>
2025-07-06 22:11:35 +0545

    updated message

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const App = () => {
   return (
     <div className="text-red-500 text-2xl font-bold text-center mt-10">
-      Hello, Tailwind is working!
+      Hey here, Tailwind is working!
     </div>
   );
 };
